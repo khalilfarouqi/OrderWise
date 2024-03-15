41657c259a02ad507d5685ebae9e283288904837
khalilfarouqi <bio.khalil.f@gmail.com>
2024-03-15 22:52:28 +0000

    Init commit

---
+++ src/main/java/com/example/orderwise/OrderWiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.orderwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderWiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderWiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/orderwise/OrderWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderWiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
