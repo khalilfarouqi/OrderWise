1c412fc2acf44397249ada0a3cb075ad02fce780
khalilfarouqi <bio.khalil.f@gmail.com>
2024-03-22 23:23:05 +0000

    remove Exception

--- src/main/java/com/example/orderwise/base/IBaseService.java
+++ src/main/java/com/example/orderwise/base/IBaseService.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.List;
 
 public interface IBaseService<E, D extends Serializable> extends IRsqlService<E, D > {
-    D save(D dto) throws Exception;
+    D save(D dto);
     D update(D dto);
     void delete(Long id);
     D findById(Long id);
