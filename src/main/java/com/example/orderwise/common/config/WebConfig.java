e17b8160fe8e89e88d6287af35442bbd0d3f5439
khalilfarouqi <bio.khalil.f@gmail.com>
2024-06-12 23:30:35 +0100

    add new. CorsMappings

--- src/main/java/com/example/orderwise/common/config/WebConfig.java
+++ src/main/java/com/example/orderwise/common/config/WebConfig.java
@@ -7,7 +7,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200") // Angular application's origin
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
