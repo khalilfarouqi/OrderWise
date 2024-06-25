210c177140e680063661554852cade493d88a1f4
khalilfarouqi <bio.khalil.f@gmail.com>
2024-06-25 20:08:29 +0100

    Add new types to NotificationType enum

--- src/main/java/com/example/orderwise/entity/enums/NotificationType.java
+++ src/main/java/com/example/orderwise/entity/enums/NotificationType.java
@@ -1,5 +1,5 @@
 package com.example.orderwise.entity.enums;
 
 public enum NotificationType {
-    SMS,MAIL,SMS_MAIL,NOTIFICATION
+    SMS,MAIL,SMS_MAIL,NOTIFICATION,NOTIFICATION_SMS,NOTIFICATION_MAIL,NOTIFICATION_SMS_MAIL
 }
