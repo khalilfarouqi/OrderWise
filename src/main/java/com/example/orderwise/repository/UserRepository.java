98327789b2bbdfd997e84bf375e160201acbb048
khalilfarouqi <bio.khalil.f@gmail.com>
2024-07-01 00:20:22 +0100

    Add methode to count by userType

--- src/main/java/com/example/orderwise/repository/UserRepository.java
+++ src/main/java/com/example/orderwise/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.orderwise.repository;
 
 import com.example.orderwise.entity.User;
+import com.example.orderwise.entity.enums.UserType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
     Optional<User> findByCin(String cin);
+    int countByUserType(UserType userType);
 }
