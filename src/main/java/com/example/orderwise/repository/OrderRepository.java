170221935b3fdc727dc1302a7cef2c19c3f720cb
khalilfarouqi <bio.khalil.f@gmail.com>
2024-07-27 21:04:12 +0100

    correct show data in dash

--- src/main/java/com/example/orderwise/repository/OrderRepository.java
+++ src/main/java/com/example/orderwise/repository/OrderRepository.java
@@ -23,17 +23,17 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("select o.confirmationDate from Order o join o.cart c join c.items i join i.product p join p.seller s where s.username = :seller and o.stage = :stage order by o.confirmationDate desc")
     Date getDateOfLastConfirmation(@Param("stage") Stage stage, @Param("seller") String sellerUsername);
-    @Query("select o.confirmationDate from Order o where o.stage = :stage order by o.confirmationDate desc")
+    @Query("select o.confirmationDate from Order o where o.stage = :stage order by o.confirmationDate desc limit 1")
     Date getDateOfLastConfirmation(@Param("stage") Stage stage);
 
     @Query("select o.deliveredDate from Order o join o.cart c join c.items i join i.product p join p.seller s where s.username = :seller and o.stage = :stage order by o.deliveredDate desc")
     Date getDateOfLastDelivered(@Param("stage") Stage stage, @Param("seller") String sellerUsername);
-    @Query("select o.deliveredDate from Order o where o.stage = :stage order by o.deliveredDate desc")
+    @Query("select o.deliveredDate from Order o where o.stage = :stage order by o.deliveredDate desc limit 1")
     Date getDateOfLastDelivered(@Param("stage") Stage stage);
 
     @Query("select o.returnDate from Order o join o.cart c join c.items i join i.product p join p.seller s where s.username = :seller and o.stage = :stage order by o.returnDate desc")
     Date getDateOfLastReturn(@Param("stage") Stage stage, @Param("seller") String sellerUsername);
-    @Query("select o.returnDate from Order o where o.stage = :stage order by o.returnDate desc")
+    @Query("select o.returnDate from Order o where o.stage = :stage order by o.returnDate desc limit 1")
     Date getDateOfLastReturn(@Param("stage") Stage stage);
 
     @Query("select o from Order o join o.cart c join c.items i join i.product p join p.seller s where s.userType = :userType and o.stage = :stage")
