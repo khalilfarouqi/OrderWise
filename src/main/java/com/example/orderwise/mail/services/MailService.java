bae36faeb3dc733829adb4a81999ee9ae79dcd3e
khalilfarouqi <bio.khalil.f@gmail.com>
2024-07-31 23:56:17 +0100

    Add new email methode

--- src/main/java/com/example/orderwise/mail/services/MailService.java
+++ src/main/java/com/example/orderwise/mail/services/MailService.java
@@ -1,16 +1,11 @@
 package com.example.orderwise.mail.services;
 
-import com.example.orderwise.common.dto.ConfigAppDto;
-import com.example.orderwise.common.dto.MyMoneyDto;
-import com.example.orderwise.common.dto.UserDto;
-import com.example.orderwise.entity.ConfigApp;
+import com.example.orderwise.common.dto.*;
 import com.example.orderwise.service.ConfigAppService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -36,6 +31,41 @@ public class MailService {
         this.mailSender = mailSender;
         this.fromEmail = fromEmail;
         this.configAppService = configAppService;
+    }
+
+    public void sendConfirmedOrderEmail(String subject, OrderDto orderDto) throws Exception {
+        Map<String, Object> model = new HashMap<>();
+        model.put("clientName", orderDto.getCustomer().getCompleteNom());
+        model.put("orderNumber", orderDto.getTrackingCode());
+        model.put("orderDate", orderDto.getOrderDate());
+        model.put("orderTotal", orderDto.getTotalPrice());
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        helper.setFrom(fromEmail);
+        helper.setTo(orderDto.getCustomer().getEmail());
+        helper.setSubject(subject);
+        helper.setText(FreeMarkerTemplateUtils.processTemplateIntoString(freemarkerConfig.getTemplate("confirmed_order.ftlh"), model), true);
+
+        mailSender.send(message);
+    }
+
+    public void sendCanceledOrderEmail(String subject, OrderDto orderDto) throws Exception {
+        Map<String, Object> model = new HashMap<>();
+        model.put("clientName", orderDto.getCustomer().getCompleteNom());
+        model.put("orderNumber", orderDto.getTrackingCode());
+        model.put("orderDate", orderDto.getOrderDate());
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        helper.setFrom(fromEmail);
+        helper.setTo(orderDto.getCustomer().getEmail());
+        helper.setSubject(subject);
+        helper.setText(FreeMarkerTemplateUtils.processTemplateIntoString(freemarkerConfig.getTemplate("canceled_order.ftlh"), model), true);
+
+        mailSender.send(message);
     }
 
     public void sendDeleteEmail(String subject, UserDto userDto) throws Exception {
@@ -78,26 +108,16 @@ public class MailService {
         mailSender.send(message);
     }
 
-    public void sendEmail(String to, String subject, Map<String, Object> model, String template) throws MessagingException {
+    public void sendEmail(String to, String subject, Map<String, Object> model, String template) throws Exception {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message);
 
-        try {
-            freemarkerConfig.setClassForTemplateLoading(this.getClass(), "/templates");
-            freemarkerConfig.setDefaultEncoding("UTF-8");
+        helper.setFrom(fromEmail);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(FreeMarkerTemplateUtils.processTemplateIntoString(freemarkerConfig.getTemplate(template), model), true);
 
-            String text = FreeMarkerTemplateUtils.processTemplateIntoString(freemarkerConfig.getTemplate(template), model);
-
-            helper.setTo(to);
-            helper.setText(text, true);
-            helper.setSubject(subject);
-            helper.setFrom(fromEmail);
-
-            mailSender.send(message);
-
-        } catch (Exception e) {
-            throw new MessagingException("Failed to send email", e);
-        }
+        mailSender.send(message);
     }
 
     public void afterSendDemandMoney(String subject, MyMoneyDto myMoneyDto) throws Exception {
