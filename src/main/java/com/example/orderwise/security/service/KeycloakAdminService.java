0fb6a9cb6ca9a2c703580efd857cc6e3f0c8a890
khalilfarouqi <bio.khalil.f@gmail.com>
2024-08-18 22:01:32 +0100

    Remove keycloak config in keycloak service and add role

--- src/main/java/com/example/orderwise/security/service/KeycloakAdminService.java
+++ src/main/java/com/example/orderwise/security/service/KeycloakAdminService.java
@@ -1,44 +1,20 @@
 package com.example.orderwise.security.service;
 
-import com.example.orderwise.security.config.AppProperties;
 import org.keycloak.admin.client.resource.UsersResource;
 import com.example.orderwise.entity.enums.UserType;
 import com.example.orderwise.common.dto.UserDto;
 import org.springframework.stereotype.Service;
-import org.keycloak.OAuth2Constants;
 import org.keycloak.representations.idm.*;
-import org.keycloak.admin.client.*;
 import org.springframework.http.*;
 
-import jakarta.ws.rs.client.*;
+import org.keycloak.admin.client.resource.*;
 
 import java.util.*;
 
+import static com.example.orderwise.security.config.JwtConverter.*;
+
 @Service
 public class KeycloakAdminService {
-
-    private final AppProperties appProperties;
-
-    static Keycloak keycloak = null;
-
-    public KeycloakAdminService(AppProperties appProperties) {
-        this.appProperties = appProperties;
-    }
-
-    public Keycloak getInstance() {
-        Client resteasyClient = ClientBuilder.newBuilder().build();
-        keycloak = KeycloakBuilder.builder()
-                .serverUrl(appProperties.getAuthServerUrl())
-                .grantType(OAuth2Constants.PASSWORD)
-                .realm(appProperties.getRealm())
-                .clientId(appProperties.getResource())
-                .username(appProperties.getUsername())
-                .password(appProperties.getPassword())
-                .resteasyClient(resteasyClient)
-                .build();
-        keycloak.tokenManager().getAccessToken();
-        return keycloak;
-    }
 
     public static CredentialRepresentation createPasswordCredentials(String password) {
         CredentialRepresentation passwordCredentials = new CredentialRepresentation();
@@ -49,29 +25,35 @@ public class KeycloakAdminService {
     }
 
     public ResponseEntity<String> createUser(UserDto userDto) {
+        //set user password
         CredentialRepresentation credential = createPasswordCredentials(userDto.getPassword());
+        //set user info
         UserRepresentation user = new UserRepresentation();
         user.setUsername(userDto.getUsername());
         user.setFirstName(userDto.getFirstname());
         user.setLastName(userDto.getLastname());
         user.setEmail(userDto.getEmail());
-        Map<String, List<String>> userAttributes = new HashMap<>();
-        userAttributes.put("userType", Collections.singletonList("ADHERENT"));
-        userAttributes.put("celluleRef", Collections.singletonList("1"));
-        user.setAttributes(userAttributes);
-        //waiting adherent role
+        //set user role
         user.setRealmRoles(Collections.singletonList(UserType.ADMIN.name()));
         user.setCredentials(Collections.singletonList(credential));
         user.setEnabled(true);
         UsersResource instance = getInstance().realm(appProperties.getRealm()).users();
-        Integer status = instance.create(user).getStatus();
-        System.out.println("status ====> "+status);
-        if(status==201){
-            instance.search(user.getUsername(),true).get(0).getId();
+        if(instance.create(user).getStatus() == 201){
+            // Get the ID of the newly created user
+            String userId = instance.search(user.getUsername(), true).get(0).getId();
+
+            // Set user role
+            UserResource userResource = instance.get(userId);
+            RoleRepresentation roleRepresentation = getInstance()
+                    .realm(appProperties.getRealm())
+                    .roles()
+                    .get(UserType.ADMIN.name())
+                    .toRepresentation();
+            userResource.roles().realmLevel().add(Collections.singletonList(roleRepresentation));
+
+            return ResponseEntity.ok().body("User created successfully with role " + UserType.ADMIN.name());
         } else {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("this user Name or mail ready exist");
         }
-
-        return ResponseEntity.ok().body("User created successfully");
     }
 }
