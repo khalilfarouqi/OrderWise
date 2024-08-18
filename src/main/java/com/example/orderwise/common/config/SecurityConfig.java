a96552aad6b474c3c909a749bb70678108dc1f4d
khalilfarouqi <bio.khalil.f@gmail.com>
2024-08-18 21:06:28 +0100

    Add new chain SecurityConfig

--- src/main/java/com/example/orderwise/common/config/SecurityConfig.java
+++ src/main/java/com/example/orderwise/common/config/SecurityConfig.java
@@ -1,12 +1,39 @@
 package com.example.orderwise.common.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import com.example.orderwise.security.config.JwtConverter;
+import org.springframework.context.annotation.*;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtConverter jwtConverter;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authz) ->authz
+                .requestMatchers("/v1/**").permitAll()
+                //.requestMatchers("/v1/**").hasAnyRole(UserType.ADMIN.name())
+                .anyRequest()
+                .authenticated());
+
+        http.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtConverter)));
+
+        return http.build();
     }
 }
