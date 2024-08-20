1221117dd03fff2def2676fb85105cf01debd431
khalilfarouqi <bio.khalil.f@gmail.com>
2024-08-20 14:16:17 +0100

    remove webhook value form SlackMessageService and put it in application yaml

--- src/main/java/com/example/orderwise/slack/service/SlackMessageService.java
+++ src/main/java/com/example/orderwise/slack/service/SlackMessageService.java
@@ -1,13 +1,17 @@
 package com.example.orderwise.slack.service;
 
+import com.example.orderwise.slack.config.SlackAppProperties;
+import lombok.RequiredArgsConstructor;
 import okhttp3.*;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
+@RequiredArgsConstructor
 @Service
 public class SlackMessageService {
-    private static final String WEBHOOK_URL = "https://hooks.slack.com/services/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";
+
+    public final SlackAppProperties slackAppProperties;
 
     public void sendSlackMessage(String message) throws IOException {
         OkHttpClient client = new OkHttpClient();
@@ -18,7 +22,7 @@ public class SlackMessageService {
                 jsonPayload, MediaType.get("application/json; charset=utf-8"));
 
         Request request = new Request.Builder()
-                .url(WEBHOOK_URL)
+                .url(slackAppProperties.getWebhookUrl())
                 .post(body)
                 .build();
 
