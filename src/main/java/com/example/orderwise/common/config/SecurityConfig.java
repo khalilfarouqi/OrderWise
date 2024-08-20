907c662b74bc2cf1020c0329e43412f9c5089012
khalilfarouqi <bio.khalil.f@gmail.com>
2024-08-20 01:40:42 +0100

    Change security filter

--- src/main/java/com/example/orderwise/common/config/SecurityConfig.java
+++ src/main/java/com/example/orderwise/common/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.orderwise.common.config;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -25,14 +26,17 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) ->authz
-                .requestMatchers("/v1/**").permitAll()
-                //.requestMatchers("/v1/**").hasAnyRole(UserType.ADMIN.name())
-                .anyRequest()
-                .authenticated());
-
-        http.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtConverter)));
+        http.csrf().disable()
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/v1/destinations/getAll").permitAll()
+                        .requestMatchers("/v1/order/trucking-order/**").permitAll()
+                        .requestMatchers("/v1/keycloak/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/v1/contactus").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/v1/user").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtConverter)));
 
         return http.build();
     }
